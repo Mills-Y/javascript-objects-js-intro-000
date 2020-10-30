d36d1dff5204fafd7ad5010ad86319d1ed877fba
Yasmine Mills <awesomeperson7777777@gmail.com>
2020-10-30 16:19:05 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,7 +7,7 @@ function updatePlaylist (playlist, artistName, songTitle){
   return playlist
 }
 
-function removeFromPlaylist (playlist, artistName, songTitle){
-  delete artistName.songTitle
+function removeFromPlaylist (playlist, artistName){
+  delete artistName.playlist
   return playlist
 }
